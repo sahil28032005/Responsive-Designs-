c597e5eda61bbc4e9636d52d81b99282d49398d7
$@#il <sahilsadekar249775@gmail.com>
2024-12-06 19:59:55 +0530

    published contents

---
+++ home/Home.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- cdn link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />  <!-- CSS only -->
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="structure.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital@1&display=swap" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+  <script src="index.js"></script>
+  <title>Document</title>
+</head>
+
+<body class="body">
+<!-- navigation -->
+<iframe class="home" style="height: 700px;width: 100%; object-fit: cover;" src="navbr.html" frameborder="0"></iframe>
+<!-- 1st banner -->
+<div>
+<iframe class="home" style="height: 700px;width: 100%; object-fit: cover;" src="index.html" frameborder="0"></iframe>
+  <span class="auto-type"></span>
+</div>
+  <!--marquee  -->
+  <div style="height: 100px;">
+    <div class="marquee">
+         
+    <marquee style=" color:red;font-size:20pt;" behavior="scroll" direction="left"><b>HURRY UP! OUR NASCENT PROGRAM HAS
+        STARTED </em></u>. <i> TO PARTICIPATE IN EVENT REGISTER NOW. 90 TEAMS REGISTERED SO FAR <u>EACH TEAM WILL GET Rs
+            500 AS SEED FUNDING</i></u></b>.</marquee>
+
+          </div>
+
+  </div>
+
+
+  <!-- hostel details -->
+  <div class="section-container">
+
+    <div class="coloumn "  ">
+      <img id="image" class="image coloumn" src="hostel1.jpg" alt="">
+        &nbsp;
+    </div>
+    <div class="coloumn content" >
+            <div class="content-container">
+                <h1><br><br><br> History</h1>
+                <p><b> In 1912, the youth hostel organisation started in Altena, Germany, as a partnership between Richard Schirrmann and businessman, Wilhelm Münker.  By 1913, 83 youth hostels were established. The movement was put on hold during the First World War, but expanded after 1920 and spread throughout Europe.  Between 1924 and 1931, hostel organisations were set up in 10 countries.  In 1930, the first youth hostel opened in Denmark. 
+                </b> </p>
+            </div>
+     </div>
+  </div>
+    <div style="height: 0px;"></div>
+    
+    <div class="section-container">
+
+     
+      <div class="coloumn content" >
+              <div class="content-container">
+                  <h1>Boys Hostel</h1>
+                  <p><b>SVERI provides hostel facility separately for boys and girls. <br>
+                    The intake capacity for boys hostel is 4000. <br>
+                    The Institute provides <br>
+                    A bed, writing table, chair, fan, tube light with a table lamp connection for each inmate. <br>
+                    RO purified drinking water. <br>
+                    Solar water heater. <br>
+                    Free WI-FI internet facility, newspaper <br>
+                    24 hours electricity backup and security <br>
+                    Gym / Sports facility is available <br>
+                    Separate study rooms <br>
+                    A separate mess is adjoining the hostel building. <br>
+                    Hostel rectors co-ordinate and control activities <br>
+                    Students are strictly required to abide by the rules of the hostel. <br>
+                    Government Scholarships sha384- <br>
+                    Dr. Panjabrao Deshmukh Hostel Maintenance Allowance up to Rs. 20,000/- <br>
+                    Hostel Maintenance Allowance up to Rs. 12,000/- For SC/ST students <br>
+                  </b></p>
+                  <p>
+                  </p>
+              </div>
+       </div>
+       <div class="coloumn " ">
+        <img class="image coloumn" src="hostel3.png" alt="">
+        &nbsp;
+    </div>
+    </div>
+    
+      <div style="height: 0px;"></div>
+
+      <div class="section-container">
+
+        <div class="coloumn " ">
+          <img class="image coloumn" src="hostel1.jpg" alt="">
+            &nbsp;
+        </div>
+        <div class="coloumn content" >
+                <div class="content-container">
+                    <h1>Girls hostel  </h1>
+                    <p><b>SVERI provides hostel facility separately for boys and girls. <br>
+                      The intake capacity for boys hostel is 4000. <br>
+                      The Institute provides <br>
+                      A bed, writing table, chair, fan, tube light with a table lamp connection for each inmate. <br>
+                      RO purified drinking water. <br>
+                      Solar water heater. <br>
+                      Free WI-FI internet facility, newspaper <br>
+                      24 hours electricity backup and security <br>
+                      Gym / Sports facility is available <br>
+                      Separate study rooms <br>
+                      A separate mess is adjoining the hostel building. <br>
+                      Hostel rectors co-ordinate and control activities <br>
+                      Students are strictly required to abide by the rules of the hostel. <br>
+                      Government Scholarships sha384- <br>
+                      Dr. Panjabrao Deshmukh Hostel Maintenance Allowance up to Rs. 20,000/- <br>
+                      Hostel Maintenance Allowance up to Rs. 12,000/- For SC/ST students <br>
+                    </b></p>
+                </div>
+         </div>
+      </div>
+        <div style="height: 20px;"></div>
+
+   <!-- facility -->
+
+   <div class="con-facility">
+    <div class="mid-facility">
+    <ul class="facility">
+      
+        <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Security Assurance</li>
+        <br>
+        <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Cleanliness</li>
+        <br>
+        <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Studying Facilities</li>   
+        <br><br>  
+    </ul>
+    
+    <ul class="facility">
+        
+     <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Health Care</li>
+     <br>
+     <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Lauandry</li>
+     <br>
+     <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> 24 hours Security</li>
+     <br><br>
+    </ul>
+    <ul class="facility">
+      <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> CCTV Surveillance</li>
+      <br>
+      <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Pure Drinking Water </li>
+      <br>
+      <li> <i class="fa-solid fa-square-check" aria-hidden="true"> </i> Hot Water Supply</li>
+      <br><br>
+    </ul>
+  </div>
+</div >
+
+<div style="height: 100px;"></div>
+
+
+
+  <!-- last banner -->
+  <div class="last-b">
+    <img src="shivajimaharaj.jfif" alt="">
+    <div class="qute">
+      "The purpose of our lives is to be happy."
+      <h3>Shiv Chhatrapati
+        Shivaji Maharaj</h3>
+    </div>
+  </div>
+<div style="height: 100px;"></div>
+  <!-- objectives -->
+  <div>
+    <div class="objectives">
+      <div class="sub-objectives">
+          <h3>OBJECTIVES:</h3>
+          <p>
+              
+      To achieve the status of premier technical institute
+      <br><br>
+      To achieve excellence on Academic, Administrative and Personality Development front through own channelized pattern of teaching-learning process
+      <br><br>
+      To develop the state of Art, Research, Development and Consultancy cell
+      <br><br>
+      To strengthen Industry-Institute interaction to provide practical industrial exposure to the students and upgradation of the faculty knowledge about advanced trends
+      <br><br>
+          </p>
+      </div>
+     
+      <div class="sub-objectives">
+          <h3>Quality policy:</h3>
+          <p>
+              
+              We are committed for academic and overall development of our students
+              <br><br>
+              By effective implementation of teaching learning process
+              <br><br>
+              By establishing respectful and pleasant behavior with the students and inculcation of culture cooperation
+              <br><br>
+              By providing ample opportunities for personality development
+              <br><br><br><br><br><br><br>
+          </p>
+      </div>
+     
+      <div class="sub-objectives">
+          
+          <p>
+              
+             <h4> ADDRESS:</h4>
+              <br>
+              P.B.No.54 Gopalpur-Ranjani Road, Pandharpur-413304, Dist: Solapur, Maharashtra, India.
+              <br><br>
+              <h4>EMAIL:</h4>
+              <br>
+              1074office@cod.sveri.ac.in
+              <br><br>
+             <h4> PHONE NO:</h4>
+              <br>
+              +917767003372
+              <br>
+              +919545553801
+              <br><br><br><br><br>
+          </p>
+      </div>
+     
+  
+      </div>
+  </div>
+
+
+  <div class="copyright">
+ 
+    <p>
+      <br> Copyright @SVERI Collage Of Engineering(Polytechnic) Pandharpur 
+      designed & developed by   Bhushan Mote, Avadhut Chavan, Sahil Sadekar, Pavan Wagh
+       Under Guidance of Mr.G.S.Misal Sir (Department of Information Technology)<br><br>
+      
+      </p>
+       
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+</body>
+
+</html>
